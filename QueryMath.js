e1731016c8eea1899343fdc7c035d29d2875715b
Quentin Ikeno <ikenoq@gmail.com>
2022-05-06 16:50:37 -1000

    Add method to calculate median>

--- QueryMath.js
+++ QueryMath.js
@@ -10,6 +10,14 @@ class QueryMath {
 			this.array.reduce((total, next) => total + next) / this.array.length
 		);
 	}
+	median() {
+		const values = this.array.sort((a, b) => a - b);
+		const val_len = values.length;
+		const middle = Math.floor(val_len / 2);
+		return val_len % 2 === 1
+			? values[middle]
+			: (values[middle - 1] + values[middle]) / 2;
+	}
 }
 
 module.exports = QueryMath;
