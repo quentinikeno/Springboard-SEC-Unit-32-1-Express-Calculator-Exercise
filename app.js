b0e1ff37737add2747719c8d8bee110f3da78bb6
Quentin Ikeno <ikenoq@gmail.com>
2022-05-06 16:27:16 -1000

    Create mean route.

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require("express");
-const res = require("express/lib/response");
 const app = express();
+const QueryMath = require("./QueryMath");
 
 app.use(express.json());
 
 // mean
-app.get("/mean", () => {
-	response = { operation: "mean" };
+app.get("/mean", (req, res) => {
+	const nums = new QueryMath(req.query.nums);
+	response = { operation: "mean", value: nums.mean() };
 	return res.json({ response });
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
 	console.log("Server started on port 3000.");
 });
